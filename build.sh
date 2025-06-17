5d5864f70fcf573e307aac8d5a53268063673003
Uni298 <uni777mk@gmail.com>
2025-06-17 14:59:22 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-javac -cp spigot-api-1.20.1.jar -d out Main.java
-jar cf Main.jar -C out .
+javac -cp spigot-api-1.20.1.jar -d out *.java
+jar cf Main.jar -C out . -C plugin.yml config.yml *.class
+echo Done!
